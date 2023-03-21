753dbe92ad9f3c554d76a35ecde1cea388b84bcd
Szabolcs Gergye <szabolcs.gergye@cyara.com>
2023-03-21 15:43:45 +0100

    init

---
+++ test/test.spec.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert
+
+describe('sayhello', function () {
+  it('test', async function () {
+    const test = 'test'
+    assert.equal('test', test)
+  })
+})
---
+++ test/test3.spec.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert
+
+describe('test3', function () {
+  it('test3', async function () {
+    const test = 'test'
+    assert.equal('test', test)
+  })
+})
